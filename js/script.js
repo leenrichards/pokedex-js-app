909849c0727fc0805d3da8d0567ab58ab9b52e9f
leenrichards <leenrichards@yahoo.com>
2021-10-20 13:00:29 -0700

    Created array of Pokemon ojects
    Array object contains:
    Pokemon name (string), height (int), and type (array of strings)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let FavoriteFood = 'Thai Food';
-document.write(FavoriteFood);
+// Array of Pokemons, with their names, heights and types 
+let pokemonList = [
+    { name: 'Diglett', height: 0.2, type: ['ground'] },
+    { name: 'Slopoke', height: 1.2, type: ['Psychic', 'Water'] },
+    { name: 'Pichu', height: 0.3, type: ['Electric'] },
+    { name: 'Sawk', height: 1.4, type: ['Fighting'] },
+    { name: 'Jynx', height: 1.4, type: ['Psychic', 'Ice'] }
+]
