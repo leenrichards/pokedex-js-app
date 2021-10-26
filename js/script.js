addba5513752a5e0bc4f4624237415a5f5fcee24
leenrichards <leenrichards@yahoo.com>
2021-10-26 06:44:28 -0700

    Added new pokemon, pokemon logo and formatted css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,80 @@
 // Array of Pokemons, with their names, heights and types 
 let pokemonList = [
-    { name: 'Diglett', height: 0.2, type: ['ground'] },
-    { name: 'Slopoke', height: 1.2, type: ['Psychic', 'Water'] },
-    { name: 'Pichu', height: 0.3, type: ['Electric'] },
-    { name: 'Sawk', height: 1.4, type: ['Fighting'] },
-    { name: 'Jynx', height: 1.4, type: ['Psychic', 'Ice'] }
+    {
+        name: 'Diglett',
+        height: 0.2,
+        type: ['ground']
+    },
+    {
+        name: 'Slopoke',
+        height: 1.2,
+        type: ['Psychic', 'Water']
+    },
+    {
+        name: 'Pichu',
+        height: 0.3,
+        type: ['Electric']
+    },
+    {
+        name: 'Metapod',
+        height: 0.7,
+        type: ['Bug']
+    },
+    {
+        name: 'Jynx',
+        height: 1.4,
+        type: ['Psychic', 'Ice']
+    },
+    {
+        name: 'Wigglytuff',
+        height: 1,
+        type: ['Fairy', 'Normal']
+    },
+    {
+        name: 'Rattata',
+        height: 0.3,
+        type: ['Normal']
+    },
+    {
+        name: 'Seel',
+        height: 1.1,
+        type: ['Water']
+    },
+    {
+        name: 'Cubone',
+        height: 0.4,
+        type: ['Ground']
+    },
+    {
+        name: 'Sudowoodo',
+        height: 1.2,
+        type: ['Rock']
+    }
 ]
+
+//set pokemon count number for new row addition in loop
+let pokemonCount = 1;
+document.write("<div class='pokemon-list'> ");
+for (let i = 1; i < pokemonList.length; i++) {
+    if (pokemonCount === 6) {
+        document.write("</div>")
+        document.write("<div class='pokemon-list'> ");
+        pokemonCount = 1;
+    }
+
+    if (pokemonList[i].height > 1.2) {
+        document.write("<div class='pokemon-list__item' style='background-color:lightyellow;'>");
+        document.write("<div class='pokemon-name'>" + pokemonList[i].name + "</div>");
+        document.write("<div class='pokemon-height' >Height:" + pokemonList[i].height + "- Wow that's tall!</div >");
+    } else {
+        document.write("<div class='pokemon-list__item'>");
+        document.write("<div class='pokemon-name'>" + pokemonList[i].name + "</div>");
+        document.write("<div class='pokemon-height' >Height:" + pokemonList[i].height + "</div >");
+    }
+    //document.write("<div class='pokemon-height'>Height:" + pokemonList[i].height + "</div>");
+    
+    document.write("</div>")
+    pokemonCount = pokemonCount + 1;
+}
+document.write("</div>")
+
